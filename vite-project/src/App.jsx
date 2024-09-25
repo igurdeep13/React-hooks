fe67fb0f61bdaf6b31c9eaba5b74fa35f1fb5949
igurdeep13 <g.s.basantpuria@gmail.com>
2024-09-24 22:27:12 -0400

    useRef state

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -46,18 +46,79 @@
 // export default App;
 
 // -------------USE EFFECT hook-------------
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+
+// function App() {
+//   const [value, setValue] = useState(0);
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setValue((value) => value + 1);
+//     }, 2000);
+//   }, [value]);
+//   return (
+//     <>
+//       <h1>I have rendered {value} times</h1>
+//     </>
+//   );
+// }
+
+// export default App;
+
+// ---------------UseRef Hook-------------
+
+// UseRef doesnot re-render the component when the state is changed
+// UseRef is also used to manipulate the dom elements
+
+// import React, { useState, useEffect, useRef } from "react";
+
+// function App() {
+//   const [value, setValue] = useState(0);
+//   // const count = useRef(0);
+//   const [count, setCount] = useState(0);
+
+//   // useEffect(() => {
+//   //   count.current = count.current + 1;
+//   // });
+
+//   useEffect(()=>{
+//     setCount(count => count + 1)
+//   },[value] )
+//   return (
+//     <>
+//       <button
+//         onClick={() => {
+//           setValue((prev) => prev - 1);
+//         }}
+//       >
+//         -
+//       </button>
+//       <h2>{value}</h2>
+//       <button
+//         onClick={() => {
+//           setValue((prev) => prev + 1);
+//         }}
+//       >
+//         +
+//       </button>
+//       <h2>Count: {count-1}</h2>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
-  const [value, setValue] = useState(0);
-  useEffect(() => {
-    setTimeout(() => {
-      setValue((value) => value + 1);
-    }, 2000);
-  }, [value]);
+  const value = useRef();
+  const btnClicked = () => {
+    console.log(value.current);
+    value.current.style.backgroundColor = "red"
+  };
   return (
     <>
-      <h1>I have rendered {value} times</h1>
+      <input type="text" ref={value} />
+      <button onClick={btnClicked}>Click</button>
     </>
   );
 }
