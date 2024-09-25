5f65c8044b1a2f6a53aae9b2e85f2ca1fb47d307
igurdeep13 <g.s.basantpuria@gmail.com>
2024-09-24 21:09:20 -0400

    UseEffect hook

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,65 @@
-import React, { useState } from "react";
+// USE STATE HOOK
+
+// import React, { useState } from "react";
+
+// function App() {
+//   const [car, setCar] = useState({
+//     make: "ferrari",
+//     model: "roma",
+//     year: "2009",
+//     color: "red",
+//   });
+
+//   function handleChange(e) {
+//     setCar((prev)=>{
+//       return {...prev, color: e.target.value}
+//     });
+//   }
+
+//   return (
+//     <>
+//       <h1>
+//         My fav Car is {car.color} {car.make} {car.model}
+//       </h1>
+//       <input
+//         onChange={(e) => {
+//           handleChange(e);
+//         }}
+//       />
+//     </>
+//   );
+// }
+
+// export default App;
+
+// import React, { useState } from "react";
+
+// function App() {
+//   const [counter, setCounter] = useState(0);
+//   return <>
+//     <h1>{counter}</h1>
+//     <button onClick={()=>{setCounter(counter+1)}}>+</button>
+//     <button onClick={()=>{setCounter(counter-1)}}>-</button>
+//   </>;
+// }
+
+// export default App;
+
+// -------------USE EFFECT hook-------------
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [car, setCar] = useState({
-    make : "ferrari",
-    model: "roma",
-    year: "2009",
-    color: "red"
-  });
-
-  return <>
-    <h1>My fav Car is {car.make} {car.model}</h1>
-    
-  </>
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setValue((value) => value + 1);
+    }, 2000);
+  }, [value]);
+  return (
+    <>
+      <h1>I have rendered {value} times</h1>
+    </>
+  );
 }
 
 export default App;
